b71ccd30b7ab4a7a96ac440289b44aa901a8a811
sooj36 <sooj2023@gmail.com>
2024-08-20 16:35:14 +0900

    [UI] 기본 UI 작업 수정 완료

--- app/src/main/java/com/sooj/today_music/presentation/WritePost.kt
+++ app/src/main/java/com/sooj/today_music/presentation/WritePost.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
@@ -18,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -28,34 +35,67 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sooj.today_music.R
+import com.sooj.today_music.Screen
 
 @Composable
 fun WritePostScreen(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column {
+            Spacer(modifier = Modifier.height(8.dp))
             Image(painterResource(id = R.drawable.back), contentDescription = "back",
-                modifier = Modifier.clickable { navController.popBackStack() })
+                modifier = Modifier
+                    .clickable { navController.popBackStack() }
+                    .size(24.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-            Row {
-                var text by remember { mutableStateOf("enter the text here") }
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(onClick = { navController.navigate(Screen.PosterList.route) }) {
+                    Text(text = "추가")
+                }
+            }
+
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                var text by remember { mutableStateOf("") }
 
                 BasicTextField(modifier = Modifier
-                    .width(50.dp)
-                    .border(color = Color.Red, width = 1.dp)
+                    .width(80.dp)
+                    .weight(8f)
+                    .border(color = Color.Transparent, width = 1.dp)
                     .background(Color.LightGray),
-                    value = text , onValueChange = { text = it },
+                    value = text, onValueChange = { text = it },
                     textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
                     singleLine = true,
                     decorationBox = { innerTextField ->
-                        Box {
-                            //
+                        Box(
+                            modifier = Modifier
+                                .padding(8.dp)
+                        ) {
+                            if (text.isEmpty()) {
+                                Text(
+                                    text = "오늘의 노래를 검색하세요", style = TextStyle(color = Color.Gray)
+                                )
+                            }
+                            innerTextField() // 실제 텍스트 입력 필드
                         }
                     }
                 )
 
+                Spacer(modifier = Modifier.width(8.dp))
+
                 Button(onClick = { /*TODO*/ }) {
+                    Modifier
+                        .weight(2f)
+                        .background(Color.Black)
+                    Text(text = "검색")
                     // 검색 api 진입 버튼
-                    
+
                 }
             }
 //
