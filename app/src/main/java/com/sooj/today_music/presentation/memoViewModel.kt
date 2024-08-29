59d414e0d4d131507deb672defeb0529190e5744
sooj36 <sooj2023@gmail.com>
2024-08-29 20:38:19 +0900

    [feat] memo viewmodel 설정

--- app/src/main/java/com/sooj/today_music/presentation/memoViewModel.kt
+++ app/src/main/java/com/sooj/today_music/presentation/memoViewModel.kt
@@ -1,5 +1,7 @@
 package com.sooj.today_music.presentation
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.sooj.today_music.domain.MemoRepository
@@ -9,7 +11,17 @@ class memoViewModel @Inject constructor(
     private val memoRepository: MemoRepository
 ) : ViewModel() {
 
-    private val _getMemo = mutableStateOf()
+    private val _memoContent = mutableStateOf("")
+    val memoContent : State<String> get() = _memoContent
+
+    suspend fun loadMemo() {
+        _memoContent.value = memoRepository.getMemo()
+    }
+
+    suspend fun saveMemo(content : String) {
+        memoRepository.saveMemo(content) //메모내용 저장
+        _memoContent.value = content // 내부상태 업데이트
+    }
 
 
 }
