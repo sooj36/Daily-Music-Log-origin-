d770c9b4de2dee98c57164c50d3bdac34dd1223d
sooj36 <sooj2023@gmail.com>
2024-09-02 19:48:01 +0900

    [feat] 데이터베이스 설정

--- app/src/main/java/com/sooj/today_music/room/Database.kt
+++ app/src/main/java/com/sooj/today_music/room/Database.kt
@@ -1,7 +1,16 @@
 package com.sooj.today_music.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
-@Database(entities = [MemoEntity::class], version = 1)
-class Database {
+@Database(entities = [TrackEntity::class], version = 1)
+abstract class Database : RoomDatabase() {
+
+    companion object {
+        const val NAME = "name_DB"
+    }
+
+    abstract fun getDao() : Dao // db에 접근 가능한 다오 설정
+
+
 }
