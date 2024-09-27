85c95ad33297d2aafa90022ae16a0aa264efd4ac
sooj36 <sooj2023@gmail.com>
2024-09-27 23:05:40 +0900

    [design] Select Page

--- app/src/main/java/com/sooj/today_music/presentation/SelectPage.kt
+++ app/src/main/java/com/sooj/today_music/presentation/SelectPage.kt
@@ -2,22 +2,33 @@ package com.sooj.today_music.presentation
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -39,32 +50,64 @@ fun SelectPageScreen(navController: NavController, musicViewModel: MusicViewMode
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .padding(25.dp)
     ) {
-        Column {
-            Text(text = "<> TRACK INFORMATION <>")
+        Column(
+            modifier = Modifier
+                .background(Color.Black)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "<> TRACK INFORMATION <>",
+                fontSize = 19.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.White
+            )
 
-            LazyVerticalGrid(columns = GridCells.Fixed(1)) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(1),
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = 16.dp) // Optional padding
+            ) {
                 item {
                     selectedTrack?.let { trackInfo ->
-                        Column(modifier = Modifier
-                            .padding(10.dp)
-                            .clickable { navController.navigate("detail_page") })
+                        Column(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .fillMaxWidth()
+                                .clickable { navController.navigate("detail_page") },
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        )
                         {
-                            //
                             if (imgURL != null) {
                                 Log.d("sj--imgURL", "image URL: ${imgURL}")
-                                AsyncImage(model = imgURL, contentDescription = "poster")
+                                AsyncImage(model = imgURL,
+                                    contentDescription = "poster",
+                                    modifier = Modifier
+                                        .size(200.dp))
                             } else {
                                 Image(
                                     painterResource(id = R.drawable.img),
                                     contentDescription = "null img"
                                 )
                             }
-                            Text(text = trackInfo?.artist ?: "아티스트")
-                            Text(text = trackInfo?.name ?: "트랙")
+                            Spacer(modifier = Modifier.height(15.dp))
+                            Text(
+                                text = trackInfo?.artist ?: "아티스트",
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Medium,
+                                color = Color.White
+                            )
+
+                            Text(
+                                text = trackInfo?.name ?: "트랙",
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Medium,
+                                color = Color.White
+                            )
                         }
-
-
                     }
                 }
             }
