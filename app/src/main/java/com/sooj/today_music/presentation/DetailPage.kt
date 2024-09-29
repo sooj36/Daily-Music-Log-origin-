10036d9b71d0cc70f484da83912f57d6d49a2453
sooj36 <sooj2023@gmail.com>
2024-09-29 16:51:31 +0900

    [fix] 1대1 대응 관계에 맞게 id 변경

--- app/src/main/java/com/sooj/today_music/presentation/DetailPage.kt
+++ app/src/main/java/com/sooj/today_music/presentation/DetailPage.kt
@@ -31,15 +31,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.sooj.today_music.R
 import com.sooj.today_music.room.MemoEntity
-import com.sooj.today_music.room.TrackEntity
 
 @Composable
 fun DetailPageScreen(
--- app/src/main/java/com/sooj/today_music/room/Entities.kt
+++ app/src/main/java/com/sooj/today_music/room/Entities.kt
@@ -7,24 +7,25 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "track_table")
 data class TrackEntity(
     @PrimaryKey(autoGenerate = true)
-    val id : Int =0,
+    val trackId : Int =0,
     val trackName : String?,
     val artistName : String?,
     val imageUrl : String?,
 )
+
+////////////////////////////////////////////
 
 @Entity(
     tableName = "memo_table",
     foreignKeys = [
         ForeignKey(
             entity = TrackEntity::class,
-            parentColumns = ["id"],
+            parentColumns = ["trackId"],
             childColumns = ["trackId"],
             onDelete = ForeignKey.CASCADE
         )
     ]
 )
-
 
 data class MemoEntity(
     @PrimaryKey val trackId : Int,
