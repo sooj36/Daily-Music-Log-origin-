4e33d84cb72902c21bea718f8817446a6339381f
sooj36 <sooj2023@gmail.com>
2024-10-01 17:27:00 +0900

    [design] posterlist 카드

--- app/src/main/java/com/sooj/today_music/presentation/PosterList.kt
+++ app/src/main/java/com/sooj/today_music/presentation/PosterList.kt
@@ -1,9 +1,11 @@
 package com.sooj.today_music.presentation
 
+import android.text.style.BackgroundColorSpan
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,9 +20,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.SystemUpdateAlt
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -119,30 +124,40 @@ fun Bookmark(navController: NavController, musicViewModel: MusicViewModel) {
     // 그리드 뷰
     LazyVerticalGrid(columns = GridCells.Fixed(2)) {
         items(getAllSaveTracks) { trackEntity ->
-            Column(
+            Card(
                 Modifier
                     .fillMaxWidth()
-                    .clickable {
-//                        navController.navigate("detail_page/${trackEntity.trackId}")
-                        navController.navigate("detail_page")
-                        // 데이터 전달
-                        musicViewModel.selectTrackEntity_vm(trackEntity)
-                    },
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
+                    .padding(5.dp)
+                    .border(1.dp, Color.Gray, RoundedCornerShape(15.dp)),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFF2F2B5A)) // 배경색 설정
             ) {
-                AsyncImage(model = trackEntity.imageUrl, contentDescription = "img")
-                trackEntity.trackId?.let { trackId ->
-                    Text(text = trackId.toString()) }
-                trackEntity.trackName?.let { Text(text = it) }
-                trackEntity.artistName?.let { Text(text = it) }
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp)
+                        .clickable {
+//                        navController.navigate("detail_page/${trackEntity.trackId}")
+                            navController.navigate("detail_page")
+                            // 데이터 전달
+                            musicViewModel.selectTrackEntity_vm(trackEntity)
+                        },
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    AsyncImage(model = trackEntity.imageUrl, contentDescription = "img")
+                    trackEntity.trackId?.let { trackId ->
+                        Text(text = trackId.toString()) }
+                    trackEntity.trackName?.let { Text(text = it) }
+                    trackEntity.artistName?.let { Text(text = it) }
 
-                // 저장 시간을 date로 변환하여 표시
-                val saveAt = trackEntity.saveAt
-                val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
-                val formattedDate = dateFormat.format(Date(saveAt))
-                Text(text = "^^ ${formattedDate} ^^")
-            }
+                    // 저장 시간을 date로 변환하여 표시
+                    val saveAt = trackEntity.saveAt
+                    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+                    val formattedDate = dateFormat.format(Date(saveAt))
+                    Text(text = "^${formattedDate}^")
+                }
+            } // card
+
         }
     }
 }
